918cf9237a093113c10f26931ac142e76159f515
chuonghm <chuonghm@umd.edu>
2022-12-02 20:19:17 +0000

    update training script

--- eg3d/script.sh
+++ eg3d/script.sh
@@ -1,3 +1,3 @@
-python train.py --outdir=~/training-runs --cfg=ffhq --data=/vulcanscratch/chuonghm/FFHQ_512_1k.zip \
-  --resume=networks/ffhq512-128.pkl \
-  --gpus=1 --batch=1 --gamma=1 --gen_pose_cond=True --neural_rendering_resolution_final=128
+python train.py --outdir=~/training-runs --cfg=ffhq --data=/mnt/localssd/eg3d/dataset_preprocessing/ffhq/FFHQ_512.zip \
+  --resume=/home/chuongh/eg3d/eg3d/networks/ffhqrebalanced512-128.pkl \
+  --gpus=8 --batch=32 --gamma=1 --gen_pose_cond=True --neural_rendering_resolution_final=128 --outdir=/sensei-fs/users/chuongh/checkpoints/eg3d
